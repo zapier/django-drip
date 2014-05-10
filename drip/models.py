3998d924e6e4522cc8d283be9607fe38b5b3c88d
Todor Velichkov <todorvelichkov89@gmail.com>
2014-05-10 10:47:11 +0300

    IS NULL filter lookup type added

--- drip/models.py
+++ drip/models.py
@@ -86,6 +86,7 @@ LOOKUP_TYPES = (
     ('endswith', 'starts with'),
     ('istartswith', 'ends with (case insensitive)'),
     ('iendswith', 'ends with (case insensitive)'),
+    ('isnull', 'is null (empty value)'),
 )
 
 class QuerySetRule(models.Model):
