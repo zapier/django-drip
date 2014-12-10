929c7f7d5448f27e3e48c651941c35ff0501f149
Bryan Helmig <bryan@bryanhelmig.com>
2014-12-09 22:51:15 -0800

    reflect the vN.N.N tag format

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ if sys.argv[-1] == 'publish':
     os.system("python setup.py sdist upload")
     args = {'version': get_version(package)}
     print("You probably want to also tag the version now:")
-    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
+    print("  git tag -a v%(version)s -m 'version v%(version)s'" % args)
     print("  git push --tags")
     sys.exit()
 
